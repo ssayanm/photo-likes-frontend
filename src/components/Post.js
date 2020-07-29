6a694d7f0efcba0f4c86295fed9463aa78236f7e
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-07-29 08:34:59 +0530

    uploading the frontend files

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,31 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const post = {
-    "id": 1,
-    "description": "Me playing squash a few years ago",
-    "likes": 20,
-    "author": null,
-    "created_at": "2020-06-08T13:11:42.095Z",
-    "updated_at": "2020-06-08T13:11:42.095Z",
-    "image": {
-        "url": "/uploads/Squash_750bdf0469.jpeg",
-    }
-}
+  id: 1,
+  description: "Me playing squash a few years ago",
+  likes: 20,
+  author: null,
+  created_at: "2020-06-08T13:11:42.095Z",
+  updated_at: "2020-06-08T13:11:42.095Z",
+  image: {
+    url: "/uploads/Squash_750bdf0469.jpeg",
+  },
+};
 
-const API_URL = 'http://localhost:1337'
+const API_URL = "http://localhost:1337";
 
-const formatImageUrl = (url) => `${API_URL}${url}`
+const formatImageUrl = (url) => `${API_URL}${url}`;
 
-export default ({description, likes, url}) => {
+export default ({ description, likes, url }) => {
+  const imageUrl =
+    process.env.NODE_ENV !== "development"
+      ? url
+      : process.env.REACT_APP_BACKEND_URL + url;
 
-    return (
-        <div className="Post">
-            <img className="Post__Image" src={formatImageUrl(url)} />
-            <h4>{description}</h4>
-            <div>
-                <span>Likes: {likes}</span>
-            </div>
-        </div>
-    )
-}
-    
+  console.log(process.env.REACT_APP_BACKEND_URL);
+  return (
+    <div className="Post">
+      <img className="Post__Image" src={imageUrl} />
+      <h4>{description}</h4>
+      <div>
+        <span>Likes: {likes}</span>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import Post from '../components/Post'
+import Post from "../components/Post";
 
 export default () => {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const getPosts = async () => {
-        const response = await fetch('http://localhost:1337/posts')
-        const data = await response.json()
-        setPosts(data)
-        }
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch("http://localhost:1337/posts");
+      const data = await response.json();
+      setPosts(data);
+    };
 
-        getPosts()
-    }, [])
-  
-    return (
-        <div className="Home">
-            {posts.map(post => (
-                <Link to={`/${post.id}`}>
-                    <Post 
-                        likes={post.likes}
-                        description={post.description}
-                        url={post.image && post.image.url}
-                    />
-                </Link>
-            ))}
-        </div>
-    );
-}
+    getPosts();
+  }, []);
+
+  return (
+    <div className="Home">
+      {posts.map((post) => (
+        <Link to={`/${post.id}`}>
+          <Post
+            key={post.id}
+            likes={post.likes}
+            description={post.description}
+            url={post.image && post.image.url}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
